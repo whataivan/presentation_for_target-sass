187386b65b94eaef065d6468be80d028fedd9e86
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-05 20:08:19 +0300

    u

---
+++ js/swiper.js
@@ -0,0 +1,71 @@
+const slider = document.querySelector(".swiper");
+
+const swiper = new Swiper(slider, {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    autoplay: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+        
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+// const swiper = new Swiper(slider, {
+//   direction: "horizontal",
+//   loop: true,
+//   centeredSlides: true,
+//   grabCursor: true,
+//   initialSlide: 5,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   breakpoints: {
+//     320: {
+//       spaceBetween: 300,
+//       speed: 500,
+//       grabCursor: true,
+//       loop: true,
+//       slidesPerView: 1,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         type: "bullets",
+//         clickable: true,
+//       },
+//     },
+
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 0,
+//       loop: true,
+//       slideToClickedSlide: true,
+//       centeredSlides: true,
+//       grabCursor: true,
+//     },
+
+//     1200: {
+//       slidesPerView: 3,
+//       spaceBetween: 40,
+//       slideToClickedSlide: true,
+//       grabCursor: true,
+//       loop: true,
+//     },
+//   },
+// });
