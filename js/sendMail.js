3c637e77576dac8b013284cf3baaa968019a9ce1
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-12 17:46:55 +0300

    u

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,51 +1,46 @@
 // import emailjs from '@emailjs/browser';
 const form = document.querySelector('.hero-form')
-const footerForm=document.querySelector('.questions-form')
 const headForm = document.querySelector('.order-form')
+const footerForm = document.querySelector('.questions-form')
+
+
 form.addEventListener('submit', sendEmail)
 footerForm.addEventListener('submit', sendEmailFooter)
 headForm.addEventListener('submit', sendEmailHead)
- const formData ={}
+const formData = {}
 
-  function sendEmail(e) {
-    e.preventDefault();
-   
-  
-    emailjs.sendForm('service_1x6tfem', 'template_t43ncvi', form, 'ZzNvGgpBwsBSFGEzB')
-      .then(() => {
-        location.href='../successfulRequest.html'
-         form.reset()
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+function sendEmail(e) {
+  e.preventDefault();
+  emailjs.sendForm('service_1x6tfem', 'template_t43ncvi', form, 'ZzNvGgpBwsBSFGEzB')
+    .then(() => {
+      location.href = '../successfulRequest.html'
+      form.reset()
+    }, (error) => {
+      console.log(error.text);
+    });
+};
 
-  function sendEmailHead(e) {
-    e.preventDefault();
-    
- 
-    emailjs.sendForm('service_1x6tfem', 'template_t43ncvi', headForm, 'ZzNvGgpBwsBSFGEzB')
-      .then((result) => {
-        headForm.reset()
-        location.href='../successfulRequest.html'
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+function sendEmailHead(e) {
+  e.preventDefault();
+  emailjs.sendForm('service_1x6tfem', 'template_t43ncvi', headForm, 'ZzNvGgpBwsBSFGEzB')
+    .then((result) => {
+      headForm.reset()
+      location.href = '../successfulRequest.html'
+    }, (error) => {
+      console.log(error.text);
+    });
+};
 
-  function sendEmailFooter(e) {
-    location.href='../successfulRequest.html'
-    
-  
-   
-   
-    emailjs.sendForm('service_1x6tfem', 'template_t43ncvi', footerForm, 'ZzNvGgpBwsBSFGEzB')
-      .then((result) => {
-        footerForm.reset()
-        location.href='../successfulRequest.html'
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+function sendEmailFooter(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_1x6tfem', 'template_t43ncvi', footerForm, 'ZzNvGgpBwsBSFGEzB')
+    .then((result) => {
+      footerForm.reset()
+      location.href = '../successfulRequest.html'
+    }, (error) => {
+      console.log(error.text);
+    });
+};
 
 // 
